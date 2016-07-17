b7859b028acd545d6244e8f1154cecb0e3638969
vbshah <vbshah1996@gmail.com>
2016-07-18 02:45:57 +0530

    new

--- playfair.py
+++ playfair.py
@@ -6,9 +6,9 @@ ls = [[''] * 5 for i in range(5)]
 main_index = 0
 data = {}
 for i in key:
-    x = int(main_index / 5)
-    y = main_index % 5
     if not done[ord(i) - ord('a')]:
+        x = int(main_index / 5)
+        y = main_index % 5
         ls[x][y] = i
         done[ord(i) - ord('a')] = 1
         main_index += 1
@@ -24,57 +24,47 @@ while c < 26:
         data[chr(c + ord('a'))] = (x, y)
     c += 1
 # ls=[input().split(' ') for i in range(5)]
-s = input("Type the string for encryption or decryption : ")
-
-if choice == 1:
-    s = s.replace('i', 'j')
-else:
-    s = s.replace('j', 'i')
-#arr=[s[i]+s[i+1] for i in range(0,len(s)-1,2)]
+s = input("Type the string for encryption or decryption : ").replce('j', 'i')
+# arr=[s[i]+s[i+1] for i in range(0,len(s)-1,2)]
 arr = []
 i = 0
-# for i in range(0,len(s)-1,2):
 while i < len(s) - 1:
     a = s[i]
     b = s[i + 1]
     if a != b:
         arr.append(a + b)
+        i += 2
     else:
         arr.append(a + 'z')
-        i -= 1
-    i += 2
+        i += 1
 if i != len(s):
     arr.append(s[len(s) - 1] + 'z')
-# for i in range(5):
-# 	for j in range(5):
-# 		print(ls[i][j],end=" ")
-# 	print()
 ans = []
 if choice == 1:
     for i in arr:
-        x1_index, y1_index = data[i[0]]
-        x2_index, y2_index = data[i[1]]
-        if x1_index == x2_index:
-            ans.append(ls[x1_index][(y1_index + 1) % 5] +
-                       ls[x2_index][(y2_index + 1) % 5])
-        elif y1_index == y2_index:
-            ans.append(ls[(x1_index + 1) % 5][y1_index] +
-                       ls[(x2_index + 1) % 5][y2_index])
+        x1, y1 = data[i[0]]
+        x2, y2 = data[i[1]]
+        if x1 == x2:
+            ans.append(ls[x1][(y1 + 1) % 5] +
+                       ls[x2][(y2 + 1) % 5])
+        elif y1 == y2:
+            ans.append(ls[(x1 + 1) % 5][y1] +
+                       ls[(x2 + 1) % 5][y2])
         else:
-            ans.append(ls[x1_index][y2_index] + ls[x2_index][y1_index])
+            ans.append(ls[x1][y2] + ls[x2][y1])
 
 else:
     for i in arr:
-        x1_index, y1_index = data[i[0]]
-        x2_index, y2_index = data[i[1]]
-        if x1_index == x2_index:
-            ans.append(ls[x1_index][(y1_index - 1) % 5] +
-                       ls[x2_index][(y2_index - 1) % 5])
-        elif y1_index == y2_index:
-            ans.append(ls[(x1_index - 1) % 5][y1_index] +
-                       ls[(x2_index - 1) % 5][y2_index])
+        x1, y1 = data[i[0]]
+        x2, y2 = data[i[1]]
+        if x1 == x2:
+            ans.append(ls[x1][(y1 - 1) % 5] +
+                       ls[x2][(y2 - 1) % 5])
+        elif y1 == y2:
+            ans.append(ls[(x1 - 1) % 5][y1] +
+                       ls[(x2 - 1) % 5][y2])
         else:
-            ans.append(ls[x1_index][y2_index] + ls[x2_index][y1_index])
+            ans.append(ls[x1][y2] + ls[x2][y1])
 
 print("Answer string is", ''.join(ans))
 
